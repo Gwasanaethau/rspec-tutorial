a274ddd1966e5969e9de38f47ebcd8a6f748662f
Mark Pokorny <iridium193@gmail.com>
2015-03-13 15:11:47 +0000

    Updated test file with assignment.

--- user_spec.rb
+++ user_spec.rb
@@ -3,6 +3,7 @@ require_relative 'user'
 describe User do
   it "should be in any roles assigned to it" do
     user = User.new
+    user.assign_role("assigned role")
     expect(user).to be_in_role("assigned role")
   end
 end
