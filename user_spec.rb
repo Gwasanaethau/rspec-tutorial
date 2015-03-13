fe8f8d27f7463b7d007982b00d4ed636a7920eee
Mark Pokorny <iridium193@gmail.com>
2015-03-13 14:48:00 +0000

    Updated test file with correct syntax.

--- user_spec.rb
+++ user_spec.rb
@@ -3,6 +3,6 @@ require_relative 'user'
 describe User do
   it "should be in any roles assigned to it" do
     user = User.new
-    expect user be_in_role("assigned role")
+    expect(user).to be_in_role("assigned role")
   end
 end
